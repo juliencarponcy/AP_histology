ea133bd73de9ed4e27408c198706378386042ab4
juliencarponcy <juliencarponcy@gmail.com>
2021-04-08 10:23:55 +0100

    Matlab commit of the previous

--- av_to_RGB.m
+++ av_to_RGB.m
@@ -1,14 +1,13 @@
-function avRGB = av_to_RGB(st,avplan)
+function avRGB = av_to_RGB(st,curr_atl,s)
     
-    avRGB = zeros(size(avplan,1),size(avplan,2),3);
-    f= waitbar(0,'Reconstructing atlas colors')
+    avRGB = zeros(size(curr_atl,1),size(curr_atl,2),3);
+    f= waitbar(0,'Reconstructing atlas colors');
     
-    for y=1:size(avplan,1)-1
+    for y=1:size(curr_atl,1)-1
 % %                     y
-
-        for x=1:size(avplan,2)-1
+        for x=1:size(curr_atl,2)-1
             
-            rgbval = hex2rgb(st.color_hex_triplet(avplan(y,x)));
+            rgbval = hex2rgb(st.color_hex_triplet(curr_atl(y,x)));
             if isa(rgbval,'double')
                 avRGB(y,x,:) = rgbval;
             elseif sum(rgbval) == 3
@@ -19,7 +18,7 @@ function avRGB = av_to_RGB(st,avplan)
                 
 
         end
-        waitbar(y/size(avplan,2),f,'Reconstructing atlas colors')
+        waitbar(y/size(curr_atl,2),f,strcat('Reconstructing atlas colors, section:',num2str(s)))
     end
     close(f)
 %     figure;imagesc(avRGB)
